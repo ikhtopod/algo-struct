be9eafe3e2af669b1b46185b0748371e40e5b6da
insoLLLent <vitaly@lifanoff.ru>
2019-08-26 19:46:44 +0300

    included google test library

--- DataStructure/Queue.h
+++ DataStructure/Queue.h
@@ -1,16 +1,11 @@
 #pragma once
 
-#include <queue>
-
 namespace insoLLLent::DataStructure {
 
 /**
 	\brief dummy queue
 */
 template<typename T>
-class Queue {
-public:
-	std::queue<T> m_queue {};
-};
+class Queue {};
 
 } // namespace insoLLLent::DataStructure
--- Test/main.cpp
+++ Test/main.cpp
@@ -1,22 +1,30 @@
+#pragma warning(push)
+#pragma warning(disable:26495)
+#include "gtest/gtest.h"
+#pragma warning(pop)
+
 #include <iostream>
 
 #include "Fibonacci.h"
 #include "Queue.h"
 
-int main(int argc, char** argv) {
+TEST(Algorithm_Fibonacci, CorrectValues) {
 	using insoLLLent::Algorithm::Fibonacci;
-	using insoLLLent::DataStructure::Queue;
 
-	Queue<int> q {};
-	q.m_queue.push(Fibonacci<5>::value);
-	q.m_queue.push(Fibonacci<6>::value);
-	q.m_queue.push(Fibonacci<7>::value);
-	q.m_queue.push(Fibonacci<8>::value);
+	EXPECT_EQ(Fibonacci<0>::value, 0);
+	EXPECT_EQ(Fibonacci<1>::value, 1);
+	EXPECT_EQ(Fibonacci<2>::value, 1);
+	EXPECT_EQ(Fibonacci<3>::value, 2);
+	EXPECT_EQ(Fibonacci<4>::value, 3);
+	EXPECT_EQ(Fibonacci<5>::value, 5);
+	EXPECT_EQ(Fibonacci<6>::value, 8);
+	EXPECT_EQ(Fibonacci<7>::value, 13);
+	EXPECT_EQ(Fibonacci<8>::value, 21);
+}
 
-	while (q.m_queue.size() != 0) {
-		std::cout << q.m_queue.front() << std::endl;
-		q.m_queue.pop();
-	}
+int main(int argc, char** argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	::testing::UnitTest::GetInstance()->Run();
 
 	return 0;
 }
