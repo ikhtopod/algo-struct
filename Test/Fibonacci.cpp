8938d1426b003af989e0390e4e4f184b930a8eb2
insoLLLent <vitaly@lifanoff.ru>
2019-08-26 19:52:48 +0300

    moved tests to separate files

---
+++ Test/Fibonacci.cpp
@@ -0,0 +1,20 @@
+#pragma warning(push)
+#pragma warning(disable:26495)
+#include "gtest/gtest.h"
+#pragma warning(pop)
+
+#include "Fibonacci.h"
+
+TEST(Algorithm_Fibonacci, CorrectValues) {
+	using insoLLLent::Algorithm::Fibonacci;
+
+	EXPECT_EQ(Fibonacci<0>::value, 0);
+	EXPECT_EQ(Fibonacci<1>::value, 1);
+	EXPECT_EQ(Fibonacci<2>::value, 1);
+	EXPECT_EQ(Fibonacci<3>::value, 2);
+	EXPECT_EQ(Fibonacci<4>::value, 3);
+	EXPECT_EQ(Fibonacci<5>::value, 5);
+	EXPECT_EQ(Fibonacci<6>::value, 8);
+	EXPECT_EQ(Fibonacci<7>::value, 13);
+	EXPECT_EQ(Fibonacci<8>::value, 21);
+}
--- Test/main.cpp
+++ Test/main.cpp
@@ -5,23 +5,6 @@
 
 #include <iostream>
 
-#include "Fibonacci.h"
-#include "Queue.h"
-
-TEST(Algorithm_Fibonacci, CorrectValues) {
-	using insoLLLent::Algorithm::Fibonacci;
-
-	EXPECT_EQ(Fibonacci<0>::value, 0);
-	EXPECT_EQ(Fibonacci<1>::value, 1);
-	EXPECT_EQ(Fibonacci<2>::value, 1);
-	EXPECT_EQ(Fibonacci<3>::value, 2);
-	EXPECT_EQ(Fibonacci<4>::value, 3);
-	EXPECT_EQ(Fibonacci<5>::value, 5);
-	EXPECT_EQ(Fibonacci<6>::value, 8);
-	EXPECT_EQ(Fibonacci<7>::value, 13);
-	EXPECT_EQ(Fibonacci<8>::value, 21);
-}
-
 int main(int argc, char** argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	::testing::UnitTest::GetInstance()->Run();
